f789a5ed27cb710f7a946df8a26c0d4cfd9ce98f
Cedric Tegenkamp <cedrictegenkamp3@gmail.com>
2021-01-25 15:46:05 +0100

    Revert "Revert "remove internal""
    This reverts commit f222231463f591d54f3a3169514072dc25e36762.

--- lib/widgets/fb_news_header.dart
+++ lib/widgets/fb_news_header.dart
@@ -2,7 +2,6 @@
 import 'package:flutter/material.dart';
 
 // Package imports:
-import 'package:meta/meta.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class FbNewsHeader extends StatelessWidget {
@@ -10,7 +9,6 @@ class FbNewsHeader extends StatelessWidget {
   final String subtitle;
   final String profilePictureUrl;
 
-  @internal
   const FbNewsHeader({
     @required this.feed,
     @required this.subtitle,
--- lib/widgets/fb_news_message.dart
+++ lib/widgets/fb_news_message.dart
@@ -1,11 +1,7 @@
 // Flutter imports:
 import 'package:flutter/material.dart';
 
-// Package imports:
-import 'package:meta/meta.dart';
-
 class FbNewsMessage extends StatelessWidget {
-  @internal
   final Map<String, dynamic> feed;
   const FbNewsMessage({
     @required this.feed,
